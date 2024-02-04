fb8a5dabae13ab720481a4c977934d5b8f1cb267
Udavith Reshanjana <146712079+Udavith-Reshanjana@users.noreply.github.com>
2024-02-04 07:35:22 +0530

    login updated

--- javaScript.js
+++ javaScript.js
@@ -1,14 +1,24 @@
 // login
 function isEmpty() {
-    var usernameVal = document.getElementsByName("username")[0].value;
-    var passwordVal = document.getElementsByName("password")[0].value;
+    var usernameField = document.getElementById("username");
+    var passwordField = document.getElementById("password");
+    
+    var usernameVal = usernameField.value;
+    var passwordVal = passwordField.value;
 
     if (usernameVal === "" && passwordVal === "") {
         alert("Please enter your Username and Password!!!");
+        usernameField.style.backgroundColor = "#ff9999";
+        passwordField.style.backgroundColor = "#ff9999";
+        return;
     } else if (usernameVal === "") {
         alert("Please enter your Username!!!");
+        usernameField.style.backgroundColor = "#ff9999";
+        return;
     } else if (passwordVal === "") {
         alert("Please enter your Password!!!");
+        passwordField.style.backgroundColor = "#ff9999";
+        return;
     } else {
         alert("Form is not empty. You can proceed with the submission.");
     }
