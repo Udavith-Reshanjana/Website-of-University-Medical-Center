f03941282eb20567762fda834b985838807e3b94
Udavith Reshanjana <146712079+Udavith-Reshanjana@users.noreply.github.com>
2024-02-07 23:27:49 +0530

    clear and login button programmed by js

--- javaScript.js
+++ javaScript.js
@@ -10,16 +10,27 @@ function isEmpty() {
         alert("Please enter your Username and Password!!!");
         usernameField.style.backgroundColor = "#ff9999";
         passwordField.style.backgroundColor = "#ff9999";
+        usernameField.focus();
         return false;
     } else if (usernameVal === "") {
         alert("Please enter your Username!!!");
         usernameField.style.backgroundColor = "#ff9999";
+        usernameField.focus();
         return false;
     } else if (passwordVal === "") {
         alert("Please enter your Password!!!");
         passwordField.style.backgroundColor = "#ff9999";
+        passwordField.focus();
         return false;
     }
     return true;
+}
+function clearMeInLogIn() {
+    var usernameField = document.getElementById("username");
+    var passwordField = document.getElementById("password");
+    usernameField.value = "";
+    passwordField.value = "";
+    usernameField.focus();
+    return false;
 }
 
